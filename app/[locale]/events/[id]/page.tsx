9d492d11ebcb9f9c2d2c70d13e9e6b6037f967a7
Franko Kulešević <franko.kule97@gmail.com>
2026-02-27 12:03:37 +0100

    chore: adding og:site_name for event detail metadata

--- app/[locale]/events/[id]/page.tsx
+++ app/[locale]/events/[id]/page.tsx
@@ -65,6 +65,7 @@ export async function generateMetadata({
       title,
       description: shortDescription,
       url,
+      siteName: "BrabantEvents",
       type: "article",
       images: [{ url: eventImage, width: 1200, height: 630, alt: title }],
     },
