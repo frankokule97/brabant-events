8b6cc117b6ce86e8ca75c6c6af87686a03e5d969
Franko Kulešević <franko.kule97@gmail.com>
2026-02-27 18:05:40 +0100

    fix: adjusting background and theme to be in dark mode for all users

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="en" className="bg-gray-950 text-white">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950 text-white`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
